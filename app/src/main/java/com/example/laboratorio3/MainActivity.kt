9e3b6c40e3f037493445d048db2ea524341634e7
Juan De Leon <dele17822@uvg.edu.gt>
2019-04-05 07:48:06 -0600

    EditText and textView

--- app/src/main/java/com/example/laboratorio3/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio3/MainActivity.kt
@@ -2,11 +2,57 @@ package com.example.laboratorio3
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 
+/**
+ * Laboratorio 3
+ * @author Juan Fernando De Leon Quezada
+ */
 class MainActivity : AppCompatActivity() {
+
+    private var isNameShown: Boolean = false
+    private lateinit var editTxt: EditText
+    private lateinit var nameTxtView: TextView
+    private lateinit var userInput: TextView
+    private lateinit var nameButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Set name button
+        nameButton = findViewById(R.id.setName)
+
+        nameButton.setOnClickListener {
+
+            if (!isNameShown){
+
+                nameTxtView = findViewById(R.id.nameTxtView)
+                editTxt = findViewById(R.id.editText)
+                userInput = findViewById(R.id.userInput)
+
+                val input: String = editTxt.text.toString()
+                userInput.text = input
+
+                nameTxtView.visibility = View.GONE
+                editTxt.visibility = View.GONE
+
+                isNameShown = true
+
+            } else {
+
+                nameTxtView.visibility = View.VISIBLE
+                editTxt.visibility = View.VISIBLE
+
+                isNameShown = false
+
+            }
+
+        }
+
     }
 }
